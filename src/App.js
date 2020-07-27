d6276893bfb3ecfce558789cc8cbcc21db4ee11f
Bruno Batalha <brunobatalha37@gmail.com>
2020-07-27 16:37:31 -0400

    Adiciona footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Menu from "./components/Menu";
 import BannerMain from "./components/BannerMain";
 import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 import dadosIniciais from "./data/dados_iniciais.json";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       <Carousel category={dadosIniciais.categorias[3]} />
       <Carousel category={dadosIniciais.categorias[4]} />
       <Carousel category={dadosIniciais.categorias[5]} />
+      <Footer />
     </div>
   );
 }
